73bee33dcb5038fe702094a253eba13e61383150
EvoX7 <creativesoultouch@gmail.com>
2022-06-20 18:05:29 +0200

    Working on milestone 3  -  remove/add cactive class on click

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+
+// Image List
+const imagesList = [
+    "https://cdn.photographycourse.net/wp-content/uploads/2022/04/Portrait-vs-Landscape-Featured-Image-3.jpg",
+    "https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg",
+    "https://cdn.photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg",
+    "https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg",
+    "https://iso.500px.com/wp-content/uploads/2021/02/Torres-Del-Paine-Sunset-By-Paul-Reiffer-2-1500x1000.jpg",
+    "https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg"
+  ];
+  
+
+
+// seleziono output
+const imageWrapper = document.getElementById("wrapper-images");
+
+
+
+for (let imageSlide = 0; imageSlide < imagesList.length; imageSlide++) {
+    let insertImage = document.createElement("img");
+    imageWrapper.append(insertImage);
+    insertImage.setAttribute("src", imagesList [imageSlide]);
+    insertImage.classList.add ("active");
+    
+}
+
+// poi immagini nascoste
+
+
+
+
+
+
+
+  

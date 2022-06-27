245db2394aacc4ace581a296f965fe390c825b8f
EvoX7 <creativesoultouch@gmail.com>
2022-06-27 20:11:24 +0200

    Update - added stop carousel button

--- js/main.js
+++ js/main.js
@@ -6,60 +6,62 @@ const imagesList = [
     "https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg",
     "https://iso.500px.com/wp-content/uploads/2021/02/Torres-Del-Paine-Sunset-By-Paul-Reiffer-2-1500x1000.jpg",
     "https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg"
-];
-
-
-// Adding 
-
-// ? Selecting html output
-const imageWrapper = document.getElementById("wrapper-images");
-
-// ? Selecting active element
-let activeElement = 1;
-
-// ! Creating loop for ImageList
-for (let imageSlide = 0; imageSlide < imagesList.length; imageSlide++) {
-
+  ];
+  
+  // Adding
+  
+  // ? Selecting html output
+  const imageWrapper = document.getElementById("wrapper-images");
+  
+  // ? Selecting active element
+  let activeElement = 1;
+  
+  // ! Creating loop for ImageList
+  for (let imageSlide = 0; imageSlide < imagesList.length; imageSlide++) {
     // * Creating a new image (element)
     const newImage = document.createElement("img");
-
+  
     // * Giving attributes to the new image (element)
     newImage.setAttribute("src", imagesList[imageSlide]);
-
+  
     // * Giving conditions to active class
     if (imageSlide == activeElement) {
-        newImage.classList.add("active");
+      newImage.classList.add("active");
     }
-
-
+  
     // * Adding element to the parent
     imageWrapper.append(newImage);
-
-}
-
-setInterval(function () {
+  }
+  
+  const sliding = setInterval(function () {
     // * Getting carousels images
-    const activeImage = document.querySelector(".carousel-current-image img.active");
-
+    const activeImage = document.querySelector(
+      ".carousel-current-image img.active"
+    );
+  
     // * Removing active class
     activeImage.classList.remove("active");
-
+  
     // * Adding images
     activeElement++;
-
+  
     // * If at list end, restart from zero
     if (activeElement === 6) {
-        activeElement = 0;
+      activeElement = 0;
     }
-
+  
     // * Adding active class
     imageSelect[activeElement].classList.add("active");
-}, 3000);
-
-// * Selecting all imgs in the html div
-const imageSelect = document.querySelectorAll(".carousel-current-image img");
-
-
-const StopBtn = document.getElementById("stop-btn");
-
-
+  }, 3000);
+  
+  // * Selecting all imgs in the html div
+  const imageSelect = document.querySelectorAll(".carousel-current-image img");
+  
+  const stopBtn = document.getElementById("btn-stop");
+  
+  // * Adding event listener to the stop button
+  stopBtn.addEventListener("click", function () {
+    clearInterval(sliding);
+    console.log("Stopped");
+  });
+  
